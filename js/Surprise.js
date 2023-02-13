bcf773e5f7309e7c963f26ff6ad8844137258e01
eggz1lla <69497366+eggz1lla@users.noreply.github.com>
2023-02-13 15:37:01 +0530

    Fixed the bug on Error

--- js/Surprise.js
+++ js/Surprise.js
@@ -7,8 +7,10 @@ function HandleSurprise(){
             document.getElementById('SurpriseDiv').style.display = 'block'
             document.getElementById('InputDiv').style.display = 'none'
             document.getElementById('SuccDiv').style.display = 'block'
+            document.getElementById('SuccDiv').style.opacity = 'opacity: 100;'
     }else{
         document.getElementById('ErrorDiv').style.display = 'block'
+        document.getElementById('ErrorDiv').style.opacity = 'opacity: 100;'
     }
 
 }
